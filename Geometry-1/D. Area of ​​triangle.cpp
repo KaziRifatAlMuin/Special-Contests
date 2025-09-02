64c952b4df06a85fe2e34caea93e3eebffa42c77
Kazi Rifat Al Muin <muin2107042@stud.kuet.ac.bd>
2025-09-02 16:03:44 +0600

    Area of a polygon

---
+++ Geometry-1/D. Area of ​​triangle.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define PI acos(-1.0)
+
+#define point pair <double,double>
+#define x first
+#define y second
+
+point operator + (point a, point b) { return {a.x + b.x, a.y + b.y}; }
+point operator - (point a, point b) { return {a.x - b.x, a.y - b.y}; }
+double operator | (point a, point b) { return a.x * b.x + a.y * b.y; }
+double operator * (point a, point b) { return a.x * b.y - a.y * b.x; }
+
+void solve(){
+    int n = 3;
+    // cin >> n;
+    vector <point> p(n);
+    double area = 0;
+    for(int i = 0; i < n; i++){
+        double x, y; cin >> x >> y;
+        p[i] = {x,y};
+    }
+    for(int i = 0; i < n; i++){
+        area += (p[i] * p[(i+1)%n]) / 2.0;
+    }
+    area = abs(area);
+    cout << fixed << setprecision(15) << area <<"\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen("area1.in", "r", stdin);
+    freopen("area1.out", "w", stdout);
+    ll t = 1, st = clock();
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    // cerr << "[Time : " << 1000 * (clock() - st) / CLOCKS_PER_SEC << " ms]\n";
+    return 0;
+}
+// https://vjudge.net/contest/741635#problem/D
